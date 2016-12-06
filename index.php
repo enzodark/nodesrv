10647bd5a81a0cc1fa3eebd736f3110de1276210
ivhysenbelli <iv.hysenbelli@gmail.com>
2016-12-06 13:55:24 +0100

    Bugs

--- index.php
+++ index.php
@@ -1,13 +1,21 @@
 <?php
 //ini_set('max_execution_time', 300);
 
-//Inicializimi
+//DATABASE CONNECTION
+$link = mysqli_connect('localhost', 'root', '','nodesrv');
+if (!$link) {
+    die('Could not connect: ' . mysqli_error());
+}
+echo 'Connected successfully';
+mysqli_report(MYSQLI_REPORT_ALL);
+
+//INICIALIZIMI
 $url = "http://80.78.76.160:3050/entitetePost" ;
 //$url = "http://192.168.1.30:3050/artikujPost" ;
 
 $ch = curl_init($url);
 
-//Parametrat Header
+//PARAMETRAT HEADER
 
 $headr = array();
 $headr[] = 'Content-type: application/json';
@@ -16,7 +24,7 @@ $headr[] = 'ndermarrjaserver: Albamedia';
 $headr[] = 'Accept: application/json';
 
 
-//RawData To Send
+//RAWDATA TO SEND
 //$rawData ='{"art":[{"MARRE":"1\\/1\\/1990","NRSEL":15,"NRCHUNK":0,"PERDORUES":""}]}';
 $rawData2 ='{"art":[{"MARRE":"1\\/1\\/2015","NRSEL":100,"NRCHUNK":0,"PERDORUES":"",""}]}';
 $rawData3='{"artikujGjendje":[{"MARRE":"1\\/1\\/2015","NRSEL":100,"NRCHUNK":0,"PERDORUES":"","MAGKODI":"","KODARTIKULLI":"","ARTBARKOD":"","DETAJIM1":"","DETAJIM2":""}]}';
@@ -26,59 +34,38 @@ $rawData3='{"artikujGjendje":[{"MARRE":"1\\/1\\/2015","NRSEL":100,"NRCHUNK":0,"P
 //Settings cURL Option
 curl_setopt($ch, CURLOPT_HTTPHEADER, $headr);
 curl_setopt($ch, CURLOPT_POST,true);
-curl_setopt($ch, CURLOPT_POSTFIELDS, $rawData2);
+//curl_setopt($ch, CURLOPT_POSTFIELDS, $rawData2);
 curl_setopt($ch, CURLOPT_POSTFIELDS, $rawData3);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+$value = "?";
+
+$query = "INSERT INTO dataTable (KODARTIKULLI,PERSHKRIMARTIKULLI,PERSHKRIMIANGARTIKULLI,KODNJESIA1,KODNJESIA2,KOEFICENTARTIKULLI,MAGAZINA,AKTIV,
+KLASA,GRUPI,NENGRUPI,TVSHKODI,VENDODHJEARTIKULLI,PERSHKRIMI,IMAZH,VLERATVSH,KODIDOGANORARTIKULLI,ARTREF,KODIFIKIMARTIKULLI,KODIFIKIMARTIKULLI2,
+DTMODIFIKIM,IDSTATUSDOK,PERPESHORE,LLOJGARANCIA,GARANCIA,) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
+
+$stmt = $link->prepare();
+
+ $stmt->bind_param('ssssssssssssssssssssssss', $KODARTIKULLI,$PERSHKRIMARTIKULLI,$PERSHKRIMIANGARTIKULLI,$KODNJESIA1,$KODNJESIA2,$KOEFICENTARTIKULLI,$MAGAZINA,$AKTIV,$KLASA,$GRUPI,$NENGRUPI,$TVSHKODI,$VENDODHJEARTIKULLI,$PERSHKRIMI,$IMAZH,$VLERATVSH,$KODIDOGANORARTIKULLI,$ARTREF,$KODIFIKIMARTIKULLI,$KODIFIKIMARTIKULLI2,$DTMODIFIKIM,$IDSTATUSDOK,$PERPESHORE,$LLOJGARANCIA,$GARANCIA);
+
 
 //Ekzektuimi
 $result = curl_exec($ch);
 //$json = file_get_contents($result);
 $obj = json_decode($result,true);
 
-
-foreach($obj['entiteteTeReja']['artikujGjendjeRi'] as $lista){
-    foreach($lista as $x => $x_value) {
-    echo "Key=" . $x . ", Value=" . $x_value;
-    echo "<br>";
-}
-}
+//PRINT THE OBJECTS
+echo "<pre>";
+print_r($obj);
+echo "</pre>";
 
 foreach($obj['entiteteTeReja']['artRi'] as $lista){
     foreach($lista as $x => $x_value) {
-    echo "Key=" . $x . ", Value=" . $x_value;
-    echo "<br>";
-}
-}
 
-include 'db.php';
-$st = mysqli_prepare($link, 'INSERT INTO dataTable (KODARTIKULLI,
-PERSHKRIMARTIKULLI,
-PERSHKRIMIANGARTIKULLI,
-KODNJESIA1,
-KODNJESIA2,
-KOEFICENTARTIKULLI,
-MAGAZINA,
-AKTIV,
-KLASA,
-GRUPI,
-NENGRUPI,
-TVSHKODI,
-VENDODHJEARTIKULLI,
-PERSHKRIMI,
-IMAZH,
-VLERATVSH,
-KODIDOGANORARTIKULLI,
-ARTREF,
-KODIFIKIMARTIKULLI,
-KODIFIKIMARTIKULLI2,
-DTMODIFIKIM,
-IDSTATUSDOK,
-PERPESHORE,
-LLOJGARANCIA,
-GARANCIA,
-) VALUES (?, ?, ?,?, ?, ?,?, ?, ?,?, ?, ?,?, ?, ?,?, ?, ?,?, ?, ?,?, ?, ?)');
-
+    $stmt->execute();
+}}
 
 //Mbyllja
 curl_close($ch);
+
 ?>
